638a06b7c68edbe6074ed949e4e4ac8d8f2a0b83
Benjamin Carrington <benjcarrington@gmail.com>
2018-12-09 13:31:04 +0000

    Script made more generic

--- gn-vim-from-lxss.sh
+++ gn-vim-from-lxss.sh
@@ -1,25 +1,31 @@
-funcition gvim() {
-    # Location of [gn]vim.exe in Windows
-    GVIM_PROG="/mnt/d/Users/BenJC/Documents/4_Software/gvim/Vim/vim81/gvim.exe"
-
-    # Location of /root in Windows
+function UNIX_2_WIN_FP() {
+    PROG=$1
+    shift
     LXSS_ROOT="C:/Users/BenJC/AppData/Local/lxss"
 
     args=()
     for arg in "$@"; do
-	arg=$(realpath $arg)
+		arg=$(realpath $arg)
 
-	if [[ $arg =~ ^/mnt/ ]]; then
-	    arg=$(echo $arg | sed 's,^/mnt/\(.\),\1:,')
-	else
-	    arg=$LXSS_ROOT$arg
-	fi
+		if [[ $arg =~ ^/mnt/ ]]; then
+			arg=$(echo $arg | sed 's#^/mnt/\(.\)#\1:#')
+		else
+			arg=$LXSS_ROOT$arg
+		fi
 
-	#arg=$(echo $arg | sed 's,/,\\,g')
-
-	args+=("$arg")
+		args+=("$arg")
     done
 
-    $GVIM_PROG $args
+    echo $args
+    $PROG $args &
 }
 
+function nvim() {
+    NVIM_PROG="/mnt/d/Users/BenJC/Documents/4_Software/Neovim/bin/nvim-qt.exe"
+    UNIX_2_WIN_FP $NVIM_PROG $@
+}
+
+function gvim() {
+    GVIM_PROG="/mnt/d/Users/BenJC/Documents/4_Software/gvim/Vim/vim81/gvim.exe"
+    UNIX_2_WIN_FP $GVIM_PROG $@
+}
